e81f7abe465a22dc83bb4a6b51d485cd16dbe932
cuong <quachcuongvfu@gmail.comm>
2022-12-07 20:14:27 +0700

    first

---
+++ demo_web_pi/Controllers/HangHoaController.cs
@@ -0,0 +1,92 @@
+﻿using demo_web_pi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demo_web_pi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hangHoas);
+        }
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hangHoaVM)
+        {
+            var hangHoa = new HangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hangHoaVM.TenHangHoa,
+                DonGia = hangHoaVM.DonGia
+            };
+            hangHoas.Add(hangHoa);
+            return (Ok(new
+            {
+                Success = true,
+                Data = hangHoa
+            })); ;
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(String id)
+        {
+            try
+            {
+                var hanghoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hanghoa == null)
+                {
+                    return NotFound();
+                }
+                return Ok(hanghoa);
+            }
+            catch {
+                return BadRequest();
+            }
+        }
+        [HttpPut("{id}")]
+        public IActionResult Edit(String id, HangHoa hangHoaEdit)
+        {
+            try
+            {
+                var hanghoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hanghoa == null)
+                {
+                    return NotFound();
+                }
+                if(id != hanghoa.MaHangHoa.ToString())
+                {
+                    return BadRequest();
+                }
+                hanghoa.TenHangHoa = hangHoaEdit.TenHangHoa;
+                hanghoa.DonGia = hangHoaEdit.DonGia;
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Remove(String id)
+        {
+            try
+            {
+                var hanghoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hanghoa == null)
+                {
+                    return NotFound();
+                }
+                hangHoas.Remove(hanghoa);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+    }
+}
---
+++ demo_web_pi/Models/HangHoa.cs
@@ -0,0 +1,11 @@
+﻿namespace demo_web_pi.Models
+{
+    public class HangHoaVM
+    {
+        public String TenHangHoa { get; set; }
+        public String DonGia { get; set; }
+    }
+    public class HangHoa : HangHoaVM {
+        public Guid MaHangHoa { get; set; }
+    }
+}
